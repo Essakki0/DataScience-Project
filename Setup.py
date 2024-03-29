c2e1c28dbab7b7d631b3b2895e8214f4052f896e
Essakki0 <essakkiappanm.0@gmail.com>
2024-03-30 01:17:56 +0530

    src folder

--- Setup.py
+++ Setup.py
@@ -1,1 +1,29 @@
-def GetRequirements[]->List[str]
+from typing import List
+from setuptools import find_packages,setup
+
+
+link='-e .'
+def GetRequirements (file_name:str)->List[str]:
+
+    '''Returns the list of requirements from file'''
+    requirements=[]
+    with open(file_name,'r') as req:
+        requirements=req.readlines()
+        requirements=[req.replace('\n','') for req in requirements]
+
+        if link in requirements:
+            requirements.remove(link)
+    
+    return requirements
+
+print(GetRequirements('requirements.txt'))
+
+setup(
+    name="ML Project",
+    version='0.1',
+    author='Essakki',
+    author_email='essakkiappanm.0@gmail.com',
+    packages=find_packages(),
+    install_requires=GetRequirements("requirements.txt")
+)
+        
