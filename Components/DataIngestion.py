63c7686bc24ac629d72a4f8042feb8d773166a59
Essakki0 <141197905+Essakki0@users.noreply.github.com>
2024-04-11 12:09:00 +0530

    Delete Components directory

--- Components/DataIngestion.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr  9 16:20:00 2024
-
-@author: 30083304
-"""
-
--- Components/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr  9 18:14:58 2024
-
-@author: 30083304
-"""
-
